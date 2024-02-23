da50d60e81418b424b2bee71196cb76d1d1c7144
RumiYo <yokoyamarumi@gmail.com>
2024-02-23 02:03:17 -0500

    complete lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,66 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    food_names=[]
+    count = 0
+    while count < len(spicy_foods): 
+        food_names.append(spicy_foods[count]["name"])
+        count += 1
+    return food_names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_food_list = []
+    count = 0
+    while count < len(spicy_foods):
+        if spicy_foods[count]["heat_level"] > 5:
+            spiciest_food_list.append(spicy_foods[count])
+        count += 1
+    return spiciest_food_list
 
 def print_spicy_foods(spicy_foods):
-    pass
+    count = 0
+    while count <len(spicy_foods):
+        print(
+            spicy_foods[count]["name"] 
+            + " ("
+            + spicy_foods[count]["cuisine"]
+            + ") | Heat Level: " 
+            + "🌶" * spicy_foods[count]["heat_level"]
+        )
+        count += 1
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    count = 0 
+    while count < len(spicy_foods):
+        if spicy_foods[count]["cuisine"] == cuisine:
+            selected_food = spicy_foods[count]
+        count += 1
+    return selected_food  
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    count = 0 
+    while count < len(spicy_foods):
+        if spicy_foods[count]["heat_level"] > 5:
+            print(
+                spicy_foods[count]["name"]
+                + " ("
+                + spicy_foods[count]["cuisine"]
+                + ") | Heat Level: "
+                + "🌶" * spicy_foods[count]["heat_level"]
+            )
+        count += 1
 
 def get_average_heat_level(spicy_foods):
-    pass
+    count = 0 
+    total_heat = 0
+    average_heat = 0
+    while count < len(spicy_foods):
+        total_heat += spicy_foods[count]["heat_level"]
+        count += 1
+    average_heat = total_heat / len(spicy_foods)
+    print(int(average_heat))
+    return int(average_heat)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
